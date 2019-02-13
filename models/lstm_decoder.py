21ac709bd8a7758fb4d4167921f3fe2e63f53f9a
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-13 20:56:09 +0000

    Change decoder training

--- models/lstm_decoder.py
+++ models/lstm_decoder.py
@@ -1,53 +1,24 @@
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 
 class LSTMDecoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
-        super().__init__()
+    def __init__(self, hidden_size, output_size):
+        super(LSTMDecoder, self).__init__()
+        self.hidden_size = hidden_size
 
-        self.emb_dim = emb_dim
-        self.hid_dim = hid_dim
-        self.output_dim = output_dim
-        self.n_layers = n_layers
-        self.dropout = dropout
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
-        self.embedding = nn.Embedding(output_dim, emb_dim)
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
 
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
-
-        self.out = nn.Linear(hid_dim, output_dim)
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, input, hidden, cell):
-        # Decoder gets as input one element at a time from each sequence
-        # input has size (batch size)
-        # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
-
-        # n directions in the decoder will both always be 1, therefore:
-        # hidden = [n layers, batch size, hid dim]
-        # context = [n layers, batch size, hid dim]
-
-        # input has now size (1, batch size)
-        input = input.unsqueeze(0)
-
-        # embedded = [1, batch size, emb dim]
-        embedded = self.dropout(self.embedding(input))
-
-        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
-
-        # output = [sent len, batch size, hid dim * n directions]
-        # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
-
-        # sent len and n directions will always be 1 in the decoder, therefore:
-        # output = [1, batch size, hid dim]
-        # hidden = [n layers, batch size, hid dim]
-        # cell = [n layers, batch size, hid dim]
-
-        prediction = self.out(output.squeeze(0))
-
-        # prediction = [batch size, output dim]
-
-        return prediction, hidden, cell
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size)
