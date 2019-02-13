27f33aa9c91a432a3aa2aa8663508cf1ee0fa8fc
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-13 20:55:59 +0000

    Change encoder implementation

--- models/lstm_encoder.py
+++ models/lstm_encoder.py
@@ -1,34 +1,20 @@
 import torch.nn as nn
+import torch
 
 
 class LSTMEncoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
-        super().__init__()
+    def __init__(self, input_size, hidden_size):
+        super(LSTMEncoder, self).__init__()
+        self.hidden_size = hidden_size
 
-        self.input_dim = input_dim
-        self.emb_dim = emb_dim
-        self.hid_dim = hid_dim
-        self.n_layers = n_layers
-        self.dropout = dropout
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
 
-        self.embedding = nn.Embedding(input_dim, emb_dim)
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, -1, self.hidden_size)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
 
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, methods_sources):
-        # methods_sources has size (batch_size, sequence_len)
-
-        # embedded has size (batch_size, sequence_len, embedding_dim)
-        embedded = self.dropout(self.embedding(methods_sources))
-
-        outputs, (hidden, cell) = self.rnn(embedded)
-
-        # outputs = [src sent len, batch size, hid dim * n directions]
-        # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
-
-        # outputs are always from the top hidden layer
-
-        return hidden, cell
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size)
