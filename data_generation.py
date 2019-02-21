98d12cb224bf4dbe459d2bdd98fc400833ae4aeb
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-21 17:20:37 +0000

    Implement generation of data

--- data_generation.py
+++ data_generation.py
@@ -1,8 +1,8 @@
-from data_extraction import get_dataset_from_dir
+from data_extraction import get_dataset_from_dir, get_tokens_dataset_from_dir
 import pickle
 
-methods_source, methods_names, method_graphs = get_dataset_from_dir(
+methods_source, methods_names, methods_graphs = get_tokens_dataset_from_dir(
     "../corpus/r252-corpus-features/")
 
-pickle.dump({'methods_source': methods_source, 'methods_names': methods_names, 'method_graphs':
-    method_graphs}, open('data/methods_data.pkl', 'wb'))
+pickle.dump({'methods_source': methods_source, 'methods_names': methods_names, 'methods_graphs':
+    methods_graphs}, open('data/methods_tokens_graphs.pkl', 'wb'))
