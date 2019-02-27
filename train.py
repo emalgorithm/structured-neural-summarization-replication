5c25a77f59826aa456e0f646e36f55d20b96490d
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-02-27 20:35:58 +0000

    Add print_every to params

--- train.py
+++ train.py
@@ -14,8 +14,7 @@ def main(model_name):
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
 
-    lang, pairs = prepare_tokens()
-    pairs = pairs if not opt.n_samples else pairs[:opt.n_samples]
+    lang, pairs = prepare_tokens(num_samples=opt.n_samples)
 
     hidden_size = 256
     encoder1 = LSTMEncoder(lang.n_words, hidden_size, opt.device).to(opt.device)
@@ -23,7 +22,8 @@ def main(model_name):
     decoder = LSTMDecoder(hidden_size, lang.n_words, opt.device, attention=opt.attention).to(
         opt.device)
     lstm2lstm = Seq2Seq(encoder1, decoder, opt.device)
-    train_iters(lstm2lstm, 500000, pairs, print_every=100, model_dir=model_dir, lang=lang)
+    train_iters(lstm2lstm, 500000, pairs, print_every=opt.print_every, model_dir=model_dir,
+                lang=lang)
 
 
 parser = argparse.ArgumentParser()
@@ -31,6 +31,7 @@ parser.add_argument('--attention', type=bool, default=False, help='whether to us
 parser.add_argument('--model_name', default="test10", help='model name')
 parser.add_argument('--device', default="cpu", help='cpu or cuda')
 parser.add_argument('--n_samples', type=int, default=None, help='Number of samples to train on')
+parser.add_argument('--print_every', type=int, default=1000, help='Number of samples to train on')
 
 opt = parser.parse_args()
 print(opt)
