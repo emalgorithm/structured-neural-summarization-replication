fc78a305bb84be3d70f78b14db806c01f2065424
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2019-03-14 22:56:37 +0000

    Change file name

--- data_generation.py
+++ data_generation.py
@@ -5,4 +5,4 @@ methods_source, methods_names, methods_graphs = get_dataset_from_dir(
     "../corpus/r252-corpus-features/")
 
 pickle.dump({'methods_source': methods_source, 'methods_names': methods_names, 'methods_graphs':
-    methods_graphs}, open('data/methods_tokens_graphs2.pkl', 'wb'))
+    methods_graphs}, open('data/methods_tokens_graphs.pkl', 'wb'))
